0cf0aa3283618f46864f6050c32e6ab3859f4454
eddie ortega <23edortega@gmail.com>
2024-09-16 12:59:18 -0500

    Add project files.

---
+++ quiz-5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz_5
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a String:");// prompts user to enter a string.
+            Console.ReadLine();// keeps the question "enter a string: displayed"
+
+            
+
+
+
+        }
+    }
+}
